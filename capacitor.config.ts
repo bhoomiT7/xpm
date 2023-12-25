45b409c3a66b4f2a6f7aef67862c7a2b7f2a368b
bhoomiT7 <135812959+bhoomiT7@users.noreply.github.com>
2023-12-25 12:10:27 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'xpm',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
